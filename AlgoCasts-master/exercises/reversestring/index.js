fb3c8f0d702b46d583c8753fe643f6b843a2add7
unknown <brcampbell001@outlook.com>
2018-04-22 22:24:06 -0500

    Reverse String Reviewed 3 Solutions

--- AlgoCasts-master/exercises/reversestring/index.js
+++ AlgoCasts-master/exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/* Reverse helper function
+
+function reverse(str) {
+    const arr = str.split('');
+    return arr.reverse().join('');
+}
+
+*/
+
+/* Reverse helper solution cleaned up
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+*/
+
+/* For loop solution with ES6 for loop syntax
+
+function reverse(str) {
+  let reversed = "";
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
+
+*/
+
+// Using reduce helper and this can wow interviewer because more complex with ES6 syntax
+
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
