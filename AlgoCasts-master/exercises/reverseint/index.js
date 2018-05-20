9ff430277f6aa0890d70e169cff60d6c925579e2
unknown <brcampbell001@outlook.com>
2018-05-20 07:45:22 -0500

    reverseint reviewed

--- AlgoCasts-master/exercises/reverseint/index.js
+++ AlgoCasts-master/exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// SOLUTION WITHOUT MATH HELPER FUNCTION
+// function reverseInt(n) {
+//   const reversed = n
+//     .toString()
+//     .split("")
+//     .reverse()
+//     .join("");
+//   if (n < 0) {
+//     return parseInt(reversed) * -1;
+//   }
+//   return parseInt(reversed);
+// }
+
+function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
